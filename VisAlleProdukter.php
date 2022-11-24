b89b0ed7dc15946d5939681c19a9b6ed9c7c723f
Sanchay13 <112164807+Sanchay13@users.noreply.github.com>
2022-11-24 09:53:34 +0100

    kommentarer

--- VisAlleProdukter.php
+++ VisAlleProdukter.php
@@ -31,25 +31,25 @@
 
 <div id="Productlist">
     <?php
-        $db = new mysqli( 
+        $db = new mysqli(  // Lager connection med databasen
             'localhost',
             'root',
             '',
             'kantine');
 
-        $sql = 'SELECT * FROM product';
+        $sql = 'SELECT * FROM product'; // Vi sender sql-spørringer om å hente alt fra tabellen "product" i databasen.
         $result = $db->query($sql);
 
-        if (isset ($_GET['delete_id'])) {
-            $delete_id = (int) $_GET['delete_id']; //sender id til samme side i URL med parameter "delete id"
-            $deletesql = "DELETE FROM product WHERE id = '$delete_id'";
+        if (isset ($_GET['delete_id'])) { // En if-statement som sjekker om lenken delete_id er satt om det er det skal følgende kode kjøres:
+            $delete_id = (int) $_GET['delete_id']; // variabel som sender id til samme side i URL med parameter "delete id"
+            $deletesql = "DELETE FROM product WHERE id = '$delete_id'"; // Variabel som sletter id fra tabellen "product" i databasen hvor id er det sammen som den id i URL-en.
             $deleteresult = $db->query($deletesql);
 
-            header("Location: ./VisAlleProdukter.php");
+            header("Location: ./VisAlleProdukter.php"); // Når delete-lenken blir trukket skal nett-siden refreshe, slik at den produkten som blir slettet skal bli borte fra siden. 
             exit();
         }
             
-        
+        // Her er tabellen som skal vise alle produkter i databasen vår.
         echo "<table>
         <tr>
         <th>Name</th>
@@ -58,7 +58,7 @@
         <th>Edit</th>
         <th>Delete</th>
         </tr>";
-        
+        // Vi lager en foreach loop som gjør om variabelen resultat om til row. For hver gang et produkt blir lagt til vil foreach loopen gjøre at tabellen oppdateres.
         foreach ($result as $row){
             echo "<tr>";
                 echo "<td>".$row['Navn']."</td>";
