b82937e5aaabd5cead04290a85ce92535e6a1622
Sanchay13 <112164807+Sanchay13@users.noreply.github.com>
2023-02-03 09:47:49 +0100

    connection endring

--- connection.php
+++ connection.php
@@ -1,7 +1,8 @@
 <?php
-$db = new mysqli(  // Lager connection med databasen
-    'localhost',
-    'root',
-    '',
-    'kantine');
+$dbServername = "localhost"; // Lager connection med databasen
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "kantine";
+
+$db = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 ?>
