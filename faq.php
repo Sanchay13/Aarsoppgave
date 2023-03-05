19e513336e2e29b0d51fdf2a0ca4f44374869dbf
Sanchay13 <112164807+Sanchay13@users.noreply.github.com>
2023-03-05 10:11:40 +0100

    sende sporsmal

--- faq.php
+++ faq.php
@@ -44,13 +44,49 @@
         <h1>Send questions:</h1>
         <br>
         <form action="" method="post" autocomplete="off">
-            <textarea class="Inputtext" placeholder="Question" type="text" name="Question" id=""></textarea>
+            <textarea class="Inputtext" name="text" placeholder="Question" type="text" name="Question" id=""></textarea>
             <br>
             <input type="submit" id="submit" name="send" value="Send">
         </form>
     </div>
 
     <br><br><br>
+
+    <?php
+        session_start();
+        if (!$_SESSION['logon']){  // Hvis log on er lik false gå til login side
+            header("Location:Login.php");
+            die();
+        }
+
+        $_Quest = '';
+
+        if(isset($_POST['send'])){
+            $ok = true;
+        
+            if(!isset($_POST['text']) || $_POST['text'] == '') { 
+                $ok = false;
+            } else {
+                $_Quest = $_POST['text'];
+            }
+
+        if($ok){
+            if($ok){ 
+
+                include_once 'connection.php';
+    
+                $sql = "INSERT INTO faq (Question) VALUES ('$_Quest');";
+                
+                $db->query($sql);
+    
+                $db->close();
+        }
+
+    }
+
+    }
+
+        ?>
 
 </body>
 <script src="script.js"></script>
